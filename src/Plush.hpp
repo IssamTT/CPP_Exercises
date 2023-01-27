618848603dea840240748ae957ac0a9bf714b9aa
Issam <issam.toure@edu.univ-eiffel.com>
2023-01-27 16:41:50 +0100

    push

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -5,12 +5,36 @@ class Plush
 private:
     /* data */
 public:
+    Plush(int);
+    int value;
+    int default_val = 10;
+    bool done = false;
+    int get_cost() const
+    {
+        if (done)
+        {
+            return value;
+        }
+        return default_val;
+    }
+
+    void set_cost(int cost)
+    {
+        done = true;
+        value = cost;
+    }
     Plush(/* args */);
     ~Plush();
 };
 
-Plush::Plush(/* args */)
+Plush::Plush(int cost)
 {
+    this->default_val = cost;
+}
+
+Plush::Plush()
+{
+    Plush(10);
 }
 
 Plush::~Plush()
--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,43 @@
 #pragma once
+#include <string>
+class PlushStore
+{
+private:
+    /* data */
+public:
+    PlushStore(std::string);
+    std::string text;
+    ~PlushStore();
+    int interet = 0;
+    int value = 0;
+    std::string get_name() const
+    {
+        return text;
+    }
+    int get_wealth_amount() const
+    {
+        return value;
+    }
+    int get_stock_size() const
+    {
+        return 0;
+    }
+    int get_debt_amount() const
+    {
+        return value + interet;
+    }
+    void loan(int credit)
+    {
+        value += credit;
+        interet += (credit * 0.1);
+    }
+};
+
+PlushStore::PlushStore(std::string text)
+{
+    this->text = text;
+}
+
+PlushStore::~PlushStore()
+{
+}
