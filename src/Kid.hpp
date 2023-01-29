4c16027ebe2f69f10305db47252eb6c7bf515ddc
Issam <issam.toure@edu.univ-eiffel.com>
2023-01-29 22:03:23 +0100

    push

--- src/Kid.hpp
+++ src/Kid.hpp
@@ -1,1 +1,46 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <PlushStore.hpp>
+#include <Plush.hpp>
+
+class Kid
+{
+private:
+    /* data */
+public:
+    int money;
+    std::string name;
+    Kid(std::string, int);
+    ~Kid();
+    std::string get_name() const
+    {
+        return name;
+    }
+    int get_money() const
+    {
+        return money;
+    }
+    void buy_plush(PlushStore &plush)
+    {
+        auto p = plush.buy(money);
+        if (p)
+        {
+            money -= p.value().get_cost();
+        }
+    }
+};
+std::ostream &operator<<(std::ostream &stream, const Kid &kid)
+{
+    return stream << kid.get_name() << " has " << kid.get_money() << " euros.";
+}
+
+Kid::Kid(std::string name, int money)
+{
+    this->money = money;
+    this->name = name;
+}
+
+Kid::~Kid()
+{
+}
--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,15 +1,24 @@
 #pragma once
 #include <string>
+#include <iostream>
+#include <optional>
+#include <vector>
+#include <Plush.hpp>
+#include <PlushStore.hpp>
+
 class PlushStore
 {
 private:
-    /* data */
 public:
     PlushStore(std::string);
     std::string text;
     ~PlushStore();
     int interet = 0;
     int value = 0;
+    int stock = 0;
+    int experience = 0;
+    std::vector<int> history;
+
     std::string get_name() const
     {
         return text;
@@ -20,7 +29,7 @@ public:
     }
     int get_stock_size() const
     {
-        return 0;
+        return stock;
     }
     int get_debt_amount() const
     {
@@ -30,6 +39,41 @@ public:
     {
         value += credit;
         interet += (credit * 0.1);
+    }
+    int make_plush(int price)
+    {
+        int save = 0;
+        if (value > 0)
+        {
+            value -= price;
+            if (value < 0)
+            {
+                save = value;
+                value = 0;
+            }
+            stock += 1;
+            experience++;
+            history.push_back((price - save) + experience);
+            return (price - save) + experience;
+        }
+        return 0;
+    }
+    int get_experience()
+    {
+        return experience;
+    }
+
+    std::optional<Plush> buy(int price)
+    {
+        auto cheapest = std::min_element(history.begin(), history.end());
+        if (price > *cheapest)
+        {
+            value += *cheapest;
+            stock--;
+            history.erase(cheapest);
+            return Plush{*cheapest};
+        }
+        return {};
     }
 };
 
