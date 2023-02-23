14da40c5beac78b86229a20e84ed8b99add5f745
Issam <issam.toure@edu.univ-eiffel.com>
2023-02-23 15:53:05 +0100

    save

--- src/ArrayNode.cpp
+++ src/ArrayNode.cpp
@@ -1,1 +1,3 @@
 #include "ArrayNode.hpp"
+
+#include "Node.hpp"
--- src/StringLeaf.hpp
+++ src/StringLeaf.hpp
@@ -1,1 +1,31 @@
 #pragma once
+#include "Leaf.hpp"
+#include "NodeKind.hpp"
+
+#include <iostream>
+#include <string>
+
+class StringLeaf : public Leaf
+{
+private:
+    std::string _word;
+
+public:
+    StringLeaf(std::string word)
+        : Leaf(NodeKind::STRING)
+        , _word { word }
+
+    {}
+
+    std::string print() const { return "\"" + _word + "\""; }
+    std::string data() { return _word; }
+
+    NodeKind kind() const { return NodeKind::STRING; }
+
+    static std::unique_ptr<StringLeaf> make_ptr(std::string value)
+    {
+        return std::make_unique<StringLeaf>(value);
+    }
+    size_t height() const { return 0; }
+    size_t node_count() const { return 0; }
+};
